ee32e8db52724f307db2458bf9205746522f6b83
Luke Leiby <lucaswleiby@gmail.com>
2021-12-21 18:32:28 -0500

    Add basic template support

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+	user = {'username': 'Luke'}
+	posts = [
+		{
+			"author": {"username": "John"},
+			"body": "It was great."
+		},
+		{
+			"author": {"username": "Luke"},
+			"body": "It was alright."
+		}
+	]
+	return render_template('index.html', title='Flask Test', user=user, posts=posts)
