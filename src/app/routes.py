411f5a96b61c49d9d8fa6252767e199e3e1a6636
Luke Leiby <lucaswleiby@gmail.com>
2021-12-21 20:05:04 -0500

    Clean up code
    - Change ' to " in src/app/routes.py
    - Remove unnecessary blank line in src/app/templates/index.html

--- src/app/routes.py
+++ src/app/routes.py
@@ -2,10 +2,10 @@ from flask import render_template
 from app import app
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-	user = {'username': 'Luke'}
+	user = {"username": "Luke"}
 	posts = [
 		{
 			"author": {"username": "John"},
@@ -16,4 +16,4 @@ def index():
 			"body": "It was alright."
 		}
 	]
-	return render_template('index.html', title='Flask Test', user=user, posts=posts)
+	return render_template("index.html", title="Flask Test", user=user, posts=posts)
